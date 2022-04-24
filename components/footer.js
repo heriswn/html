777367a53234211455935496e35c9344f608f334
Heri Setiawan <heri15002@mai.unpad.ac.id>
2022-04-24 16:33:31 +0700

    init

---
+++ components/footer.js
@@ -0,0 +1,75 @@
+const footerTemplate = document.createElement("template");
+
+footerTemplate.innerHTML = `
+  <style>
+    footer {
+      height: 60px;
+      padding: 0 10px;
+      list-style: none;
+      display: flex;
+      flex-shrink: 0;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #dfdfe2;
+    }
+
+    ul {
+      padding: 0;
+    }
+
+    ul li {
+      list-style: none;
+      display: inline;
+    }
+
+    a {
+      margin: 0 15px;
+      color: inherit;
+      text-decoration: none;
+    }
+
+    a:hover {
+      padding-bottom: 5px;
+      box-shadow: inset 0 -2px 0 0 #333;
+    }
+
+    .social-row {
+      font-size: 20px;
+    }
+
+    .social-row li a {
+      margin: 0 15px;
+    }
+  </style>
+  <footer>
+    <ul>
+      <li><a href="about.html">About</a></li>
+      <li><a href="work.html">Work</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+    <ul class="social-row">
+      <li><a href="https://github.com/my-github-profile"><i class="fab fa-github"></i></a></li>
+      <li><a href="https://twitter.com/my-twitter-profile"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/my-linkedin-profile"><i class="fab fa-linkedin"></i></a></li>
+    </ul>
+  </footer>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+    const shadowRoot = this.attachShadow({ mode: "closed" });
+
+    if (fontAwesome) {
+      shadowRoot.appendChild(fontAwesome.cloneNode());
+    }
+
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define("footer-component", Footer);
